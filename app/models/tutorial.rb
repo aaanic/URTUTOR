0aa4c40607f26ba6204ccbe11a782070bdf65baa
Anic Fraefel <anicfraefel@gmail.com>
2019-08-19 13:31:58 +0200

    added the relations between models

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,3 +1,4 @@
 class Tutorial < ApplicationRecord
   belongs_to :user
+  has_many :lessons
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :tutorials
+  has_many :tutorials, through: :lessons
+  has_many :lessons
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
