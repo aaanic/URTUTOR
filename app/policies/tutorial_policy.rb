06aa2188a73a034f446c2fb12fc79c9eed94c14d
Christine Liu <christine.liu@essec.edu>
2019-08-20 15:54:00 +0200

    Added edit update destroy to pundit

--- app/policies/tutorial_policy.rb
+++ app/policies/tutorial_policy.rb
@@ -5,15 +5,33 @@ class TutorialPolicy < ApplicationPolicy
     end
   end
 
-  def new?
-    true
-  end
+  # def new?
+  #   true
+  # end
 
   def show?
     true
   end
 
   def create?
+    user_is_logged?
+  end
+
+  def edit?
+    user_is_logged?
+  end
+
+  def update?
+    user_is_logged?
+  end
+
+  def destroy?
+    user_is_logged?
+  end
+
+  private
+
+  def user_is_logged?
     record.user == user
   end
 end
