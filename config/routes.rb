1b60f77bf1c66d2f5ebd471114caece8fa00907d
Anic Fraefel <anicfraefel@gmail.com>
2019-08-22 15:35:48 +0200

    final commit before push

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :tutorials
-  resources :lessons, only: [:new, :create]
+  resources :lessons, only: [:new, :create, :destroy]
   get '/dashboard', to: 'pages#user_dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
